40bdb577087344e6dabb3acd97e7f80a1c80598c
Ethan Hunter <24462157+Racnera@users.noreply.github.com>
2019-03-26 15:12:29 -0400

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.IO;
 namespace u2Summative
 {
     /// <summary>
@@ -23,25 +23,70 @@ namespace u2Summative
         public MainWindow()
         {
             InitializeComponent();
+            int temp = 1;
+            Start:
+                Contact c = new Contact();
+                txtIn.Text = c.ReadFromFile();
+                StreamReader sr = new StreamReader("Contact.txt");
+                string[] inSplit = txtIn.Text.Split(',');
+                int counter = 0;
+                
+                for (int i = 0; i >= inSplit.Length; i++)
+                {
+                bool b = Int32.TryParse(inSplit[i], out Int32 result);
+                    if (b == true)
+                    {
+                        counter++;
+                    }
+                MessageBox.Show(counter.ToString());
+                }
+                if (counter != 3) { MessageBox.Show("OOps! Soimething went wrong!"); goto Start; }
+                if (counter == 3) { MessageBox.Show("Nega"); }
+
         }
 
         private void btnAge_Click(object sender, RoutedEventArgs e)
         {
-            
+
         }
     }
-    class Contact
+    public class Contact
     {
-        public string fName;
-        public string lName;
-        public double bDay;
-        public double bMonth;
-        public double bYear;
-        public string email;
+        string filecontent;
+        string[] conSplit;
+
+        public string ReadFromFile()
+        {
+            StreamReader sr = new StreamReader("Contact.txt");
+            while (!sr.EndOfStream)
+            {
+                filecontent = sr.ReadLine();
+
+            }
+            return filecontent;
+        }
+
 
         public Contact(string f, string l, int y, int m, int d, string e)
         {
-           
+            conSplit = filecontent.Split(',');
+            f = conSplit[0];
+            l = conSplit[1];
+            y = Int32.Parse(conSplit[2]);
+            m = Int32.Parse(conSplit[3]);
+            d = Int32.Parse(conSplit[4]);
+            e = conSplit[5];
         }
+        public Contact(string f, string l, string e)
+        {
+
+        }
+        public Contact(string f, string l)
+        {
+
+        }
+        public Contact() { }
+
     }
 }
+
